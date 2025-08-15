7cc89f5c2e0f85d44569cd3346fe409ae8bbd024
DN-kumlungrean <66050986@kmitl.ac.th>
2025-08-15 12:30:06 +0700

    แก้ background

--- Clouds.java
+++ Clouds.java
@@ -1,22 +1,15 @@
-import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.geom.CubicCurve2D;
-import java.awt.geom.Ellipse2D;
 import java.awt.geom.GeneralPath;
-import java.awt.geom.Line2D;
-import java.awt.geom.QuadCurve2D;
 
 public class Clouds {
     public void draw(Graphics2D g2d, int panelWidth) {
         g2d.setColor(new Color(255, 245, 200, 200));
         GeneralPath cloudMass = new GeneralPath();
-        cloudMass.moveTo(0, 250);
-        cloudMass.quadTo(80, 150, 180, 230);
-        cloudMass.quadTo(250, 120, 350, 220);
-        cloudMass.quadTo(450, 160, 550, 235);
-        cloudMass.quadTo(600, 180, panelWidth, 250);
+        cloudMass.moveTo(0, 200);
+        cloudMass.quadTo(80, 100, 180, 180);
+        cloudMass.quadTo(250, 70, 350, 170);
+        cloudMass.quadTo(450, 110, 550, 185);
+        cloudMass.quadTo(600, 130, panelWidth, 200);
         cloudMass.closePath();
         g2d.fill(cloudMass);
     }
--- Sand.java
+++ Sand.java
@@ -1,26 +1,18 @@
-import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.geom.CubicCurve2D;
 import java.awt.geom.Ellipse2D;
-import java.awt.geom.GeneralPath;
-import java.awt.geom.Line2D;
-import java.awt.geom.QuadCurve2D;
 
 public class Sand {
     public void draw(Graphics2D g2d, int panelWidth, double characterX) {
         g2d.setColor(new Color(244, 214, 181));
-        g2d.fillRect(0, 420, panelWidth, 180);
-
+        g2d.fillRect(0, 350, panelWidth, 250);
         g2d.setColor(new Color(218, 184, 143));
         for (int i = 0; i < 5; i++) {
             double footX = characterX - 50 - (i * 60);
             if (footX > 0) {
-                g2d.fill(new Ellipse2D.Double(footX, 500, 8, 10));
-                g2d.fill(new Ellipse2D.Double(footX - 5, 495, 4, 4));
-                g2d.fill(new Ellipse2D.Double(footX, 495, 4, 4));
-                g2d.fill(new Ellipse2D.Double(footX + 5, 495, 4, 4));
+                g2d.fill(new Ellipse2D.Double(footX, 470, 8, 10));
+                g2d.fill(new Ellipse2D.Double(footX - 5, 465, 4, 4));
+                g2d.fill(new Ellipse2D.Double(footX, 465, 4, 4));
+                g2d.fill(new Ellipse2D.Double(footX + 5, 465, 4, 4));
             }
         }
     }
